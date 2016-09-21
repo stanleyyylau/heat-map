9c3a8bab3efec6855551c40164534860ad9d6b7c
Stanley <stanleyyylau@gmail.com>
2016-09-21 17:17:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hi');
+
+var monthStr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var celStr = '°C';
+
+var scale = {};
+
+console.log(celStr);
